5810c17f4a32881cf0725833c773ffeee1b16a91
Andriana <37633606+prudyvusandriana@users.noreply.github.com>
2018-06-12 20:44:32 +0300

    Update cool_tasks.sql
    i've removed the result after running commands in terminal

--- cool_tasks.sql
+++ cool_tasks.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE cool_tasks;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION
 CREATE TABLE users ( 
 id uuid DEFAULT uuid_generate_v1(),
 name VARCHAR(34) NOT NULL,
@@ -8,7 +7,7 @@ login VARCHAR(34) NOT NULL,
 password VARCHAR(16) NOT NULL,
 PRIMARY KEY (id)
 );
-CREATE TABLE
+
 CREATE TABLE tasks (
 id uuid DEFAULT uuid_generate_v1(),
 user_id uuid REFERENCES users (id),
@@ -19,5 +18,4 @@ updated_at TIMESTAMP,
 description TEXT,
 PRIMARY KEY(id)
 );
-CREATE TABLE
 
