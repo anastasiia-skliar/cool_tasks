c24f42c6add1c1c024cccfc63be2d34d34332081
YaromyrO <oryshchyn97@gmail.com>
2018-06-14 17:21:05 +0300

    Changed users and tasks

--- src/models/taskModel.go
+++ src/models/taskModel.go
@@ -3,13 +3,14 @@ package models
 import (
 	"github.com/satori/go.uuid"
 	"time"
+	. "github.com/Nastya-Kruglikova/cool_tasks/src/database"
 )
 
 const (
-	createTask = "INSERT INTO task (user_id, name, time, created_at, updated_at, desc) VALUES ($1, $1, $3, $4, $5, $6)"
-	getTask    = "SELECT * FROM task WHERE id = $1"
-	deleteTask = "DELETE FROM task WHERE id = $1"
-	getTasks   = "SELECT * FROM task"
+	createTask = "INSERT INTO tasks (user_id, name, time, created_at, updated_at, description) VALUES ($1, $2, $3, $4, $5, $6)"
+	getTask    = "SELECT * FROM tasks WHERE id = $1"
+	deleteTask = "DELETE FROM tasks WHERE id = $1"
+	getTasks   = "SELECT * FROM tasks"
 )
 
 //Task representation in DB
@@ -24,35 +25,35 @@ type Task struct {
 }
 
 //CreateTask used for creation task in DB
-func CreateTask(task Task) error {
-	_, err := db.Exec(createTask, &task.UserID, task.Name, task.Time, task.CreatedAt, task.UpdatedAt, task.Desc)
+var CreateTask = func (task Task) error {
+	_, err := DB.Exec(createTask, &task.UserID, task.Name, task.Time, task.CreatedAt, task.UpdatedAt, task.Desc)
 
 	return err
 }
 
 //GetTask used for getting task from DB
-func GetTask(id uuid.UUID) (Task, error) {
-	task := Task{}
-	err := db.QueryRow(getTask, id).Scan(&task.ID, &task.UserID, &task.Name, &task.Time, &task.CreatedAt, &task.UpdatedAt, &task.Desc)
+var GetTask = func (id uuid.UUID) (Task, error) {
+	var task Task
+	err := DB.QueryRow(getTask, id).Scan(&task.ID, &task.UserID, &task.Name, &task.Time, &task.CreatedAt, &task.UpdatedAt, &task.Desc)
 
 	return task, err
 }
 
 //UpdateTask used fro updating task in DB
-func UpdateTask() {
+var UpdateTask = func () {
 
 }
 
 //DeleteTask used for deleting task from DB
-func DeleteTask(id uuid.UUID) error {
-	_, err := db.Exec(deleteTask, id)
+var DeleteTask = func (id uuid.UUID) error {
+	_, err := DB.Exec(deleteTask, id)
 
 	return err
 }
 
 //GetTasks used for getting tasks from DB
-func GetTasks() ([]Task, error) {
-	rows, err := db.Query(getTasks)
+var GetTasks = func () ([]Task, error) {
+	rows, err := DB.Query(getTasks)
 	if err != nil {
 		return []Task{}, err
 	}
