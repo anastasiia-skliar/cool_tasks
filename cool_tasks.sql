30884d6516cfbd7dc9ce6f9cfa1aeba8cb77557c
YaromyrO <oryshchyn97@gmail.com>
2018-06-22 12:18:23 +0300

    Updated sql file

--- cool_tasks.sql
+++ cool_tasks.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE cool_tasks;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE cool_tasks.users ( 
+CREATE TABLE users (
 id uuid DEFAULT uuid_generate_v1(),
 name VARCHAR(34) NOT NULL,
 login VARCHAR(34) NOT NULL,
@@ -8,9 +8,9 @@ password VARCHAR(16) NOT NULL,
 PRIMARY KEY (id)
 );
 
-CREATE TABLE cool_tasks.tasks (
+CREATE TABLE tasks (
 id uuid DEFAULT uuid_generate_v1(),
-user_id uuid REFERENCES users (id),
+user_id uuid REFERENCES users (id) ON DELETE CASCADE,
 name VARCHAR(34) NOT NULL,
 time TIMESTAMP,
 created_at TIMESTAMP,
