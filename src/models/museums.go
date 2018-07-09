80b17570398685031ef9620d3bbf77aac84b781f
Karim9871 <karimlind123@gmail.com>
2018-07-09 14:37:06 +0300

    used github libs

--- src/models/museums.go
+++ src/models/museums.go
@@ -4,7 +4,8 @@ import (
 	"github.com/satori/go.uuid"
 	. "github.com/Nastya-Kruglikova/cool_tasks/src/database"
 	"net/url"
-	"strings"
+	sq "github.com/Masterminds/squirrel"
+	"fmt"
 	"errors"
 )
 
@@ -47,7 +48,10 @@ var GetMuseumsByTrip = func(trip_id uuid.UUID) ([]Museum, error) {
 }
 
 var GetMuseumsByRequest = func(params url.Values) ([]Museum, error) {
-	request := "SELECT * FROM museums WHERE "
+	museums := sq.StatementBuilder.PlaceholderFormat(sq.Dollar).Select("*").From("museums")
+	var request string
+	var err error
+	var b sq.And
 	count := 0
 	validKeys := []string{"id", "name", "location", "price", "museum_type", "opened_at", "closed_at"}
 	for key, value := range params {
@@ -57,59 +61,52 @@ var GetMuseumsByRequest = func(params url.Values) ([]Museum, error) {
 			}
 		}
 		if count == 0 {
-
 			return []Museum{}, errors.New("ERROR: Bad request")
 		}
 		switch key {
 		case "name", "location", "museum_type":
 			if len(value) > 1 {
-				request += key + " IN ("
-				for i, v := range value {
-					v = "'" + v + "'"
-					request += v
-					if i < len(value)-1 {
-						request += ", "
-					}
+				var or sq.Or
+				for _, v := range value {
+					or = append(or, sq.Eq{key: v})
 				}
+				b = append(b, or)
 				request += ") AND "
 			} else {
-				value[0] = "'" + value[0] + "'"
-				request += key + "=" + value[0] + " AND "
+				b = append(b, sq.Eq{key: value[0]})
 			}
 		case "price", "opened_at", "closed_at":
 			if len(value) == 2 {
-				request += key + " BETWEEN " + value[0] + " AND " + value[1] + " AND "
+				b = append(b, sq.And{sq.GtOrEq{key: value[1]}, sq.LtOrEq{key: value[0]}})
 			} else {
-				request += key + "=" + value[0] + " AND "
+				b = append(b, sq.Eq{key: value[0]})
+
 			}
 		default:
-			request += key + "=" + value[0] + " AND "
+			b = append(b, sq.Eq{key: value[0]})
 		}
 
 		count = 0
 	}
 
-	words := strings.Fields(request)
-
-	if words[len(words)-1] == "AND" || words[len(words)-1] == "WHERE" {
-		words[len(words)-1] = ""
+	request, _, err = museums.Where(b).ToSql()
+	if err != nil {
+		fmt.Println(err)
 	}
-
-	request = strings.Join(words, " ")
-	request+=";"
+	b = nil
 	rows, err := DB.Query(request)
 	if err != nil {
 		return []Museum{}, err
 	}
 
-	museums := make([]Museum, 0)
+	museumsArray := make([]Museum, 0)
 
 	for rows.Next() {
 		var m Museum
 		if err := rows.Scan(&m.ID, &m.Name, &m.Location, &m.Price, &m.OpenedAt, &m.ClosedAt, &m.MuseumType, &m.Info); err != nil {
 			return []Museum{}, err
 		}
-		museums = append(museums, m)
+		museumsArray = append(museumsArray, m)
 	}
-	return museums, nil
+	return museumsArray, nil
 }
