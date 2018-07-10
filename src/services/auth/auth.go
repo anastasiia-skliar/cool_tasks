2ce82c5cc8fa11d3554baa6136cd8b6a6c2a8cff
Your Grandma <dmytro.chalyi.corp@gmail.com>
2018-07-10 11:16:57 +0300

    redis unmocked

--- src/services/auth/auth.go
+++ src/services/auth/auth.go
@@ -4,19 +4,15 @@ import (
 	"errors"
 	"github.com/Nastya-Kruglikova/cool_tasks/src/models"
 	"github.com/Nastya-Kruglikova/cool_tasks/src/services/common"
-	"github.com/alicebob/miniredis"
 	"github.com/satori/go.uuid"
 	"net/http"
 	"time"
+	"github.com/Nastya-Kruglikova/cool_tasks/src/database"
 )
 
 var GetUserByLogin = models.GetUserByLogin
 
-var redis *miniredis.Miniredis
-
-func init() {
-	redis, _ = miniredis.Run()
-}
+var redis = database.Cache
 
 
 type login struct {
@@ -34,6 +30,18 @@ type User struct {
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
+	userSession, _ := r.Cookie("user_session")
+	if redis.Get(userSession.Value)!=nil{
+		login := redis.Get(userSession.Value)
+		redis.Del(userSession.Value)
+		redis.Set(userSession.Value, login, time.Hour*4)
+
+		newCookie := http.Cookie{Name: "user_session", Value:userSession.Value, Expires: time.Now().Add(time.Hour * 4)}
+		http.SetCookie(w, &newCookie)
+
+		common.RenderJSON(w, r, userSession.Value)
+		return
+	}
 
 	var newLogin login
 
@@ -59,7 +67,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	}
 	if newLogin.sessionID != "" {
 
-		redis.Push(newLogin.sessionID, newLogin.login)
+		redis.Set(newLogin.sessionID, newLogin.login, time.Hour*4)
 
 		newCookie := http.Cookie{Name: "user_session", Value: newLogin.sessionID, Expires: time.Now().Add(time.Hour * 4)}
 		http.SetCookie(w, &newCookie)
