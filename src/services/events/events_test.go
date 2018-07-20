0180ab968e93937991499dd9223982197ef77b57
Unknown <danyliukandrii1408@gmail.com>
2018-07-20 19:22:13 +0300

    upd events_test coverage to 93%

--- src/services/events/events_test.go
+++ src/services/events/events_test.go
@@ -2,8 +2,10 @@ package events_test
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/Nastya-Kruglikova/cool_tasks/src/models"
 	"github.com/Nastya-Kruglikova/cool_tasks/src/services"
+	"github.com/satori/go.uuid"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -27,15 +29,29 @@ func TestGetByRequestHandler(t *testing.T) {
 			want:            200,
 			mockedGetEvents: []models.Event{},
 		},
+		{
+			name:            "Get_Events_404",
+			url:             "/v1/events?mock=890",
+			want:            404,
+			mockedGetEvents: []models.Event{},
+		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			models.MockedGetEvents()
 			rec := httptest.NewRecorder()
 			req, _ := http.NewRequest(http.MethodGet, tc.url, nil)
-
+			if tc.name == "Get_Events_404" {
+				mock := func() {
+					var err = http.ErrBodyNotAllowed
+					models.GetEventsByRequest = func(values url.Values) ([]models.Event, error) {
+						return []models.Event{}, err
+					}
+				}
+				mock()
+			}
 			router.ServeHTTP(rec, req)
-
+			fmt.Println(rec.Code)
 			if rec.Code != tc.want {
 				t.Errorf("Expected: %d , got %d", tc.want, rec.Code)
 			}
@@ -50,21 +66,86 @@ func TestAddToTripHandler(t *testing.T) {
 			url:  "/v1/events",
 			want: 200,
 		},
+		{
+			name: "Add_Events_400",
+			url:  "/v1/events",
+			want: 400,
+		},
+		{
+			name: "Add_Events_400_2",
+			url:  "/v1/events",
+			want: 400,
+		},
+		{
+			name: "Add_Events_400_3",
+			url:  "/v1/events",
+			want: 400,
+		},
 	}
-	data := url.Values{}
-	data.Add("event_id", "00000000-0000-0000-0000-000000000001")
-	data.Add("trip_id", "00000000-0000-0000-0000-000000000001")
+
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			models.MockedAddEventToTrip()
-			rec := httptest.NewRecorder()
-			req, _ := http.NewRequest(http.MethodPost, tc.url, bytes.NewBufferString(data.Encode()))
-			req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+			switch tc.name {
+			case "Add_Events_400":
+				data := url.Values{}
+				data.Add("event_id", "asdsad")
+				data.Add("trip_id", "00000000-0000-0000-0000-000000000001")
+				models.MockedAddEventToTrip()
+				rec := httptest.NewRecorder()
+				req, _ := http.NewRequest(http.MethodPost, tc.url, bytes.NewBufferString(data.Encode()))
+				req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+				router.ServeHTTP(rec, req)
+				fmt.Println(rec.Code)
+				if rec.Code != tc.want {
+					t.Errorf("Expected: %d , got %d", tc.want, rec.Code)
+				}
+			case "Add_Events_400_2":
+				data := url.Values{}
+				data.Add("event_id", "00000000-0000-0000-0000-000000000001")
+				data.Add("trip_id", "asdasd")
+				models.MockedAddEventToTrip()
+				rec := httptest.NewRecorder()
+				req, _ := http.NewRequest(http.MethodPost, tc.url, bytes.NewBufferString(data.Encode()))
+				req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+				router.ServeHTTP(rec, req)
+				fmt.Println(rec.Code)
+				if rec.Code != tc.want {
+					t.Errorf("Expected: %d , got %d", tc.want, rec.Code)
+				}
 
-			router.ServeHTTP(rec, req)
+			case "Add_Events_400_3":
+				data := url.Values{}
+				data.Add("event_id", "00000000-0000-0000-0000-000000000001")
+				data.Add("trip_id", "00000000-0000-0000-0000-000000000001")
+				mock := func() {
+					var err = error(new(http.ProtocolError))
+					models.AddEventToTrip = func(event_id uuid.UUID, trip_id uuid.UUID) error {
+						return err
+					}
+				}
+				mock()
+				rec := httptest.NewRecorder()
+				req, _ := http.NewRequest(http.MethodPost, tc.url, bytes.NewBufferString(data.Encode()))
+				req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+				router.ServeHTTP(rec, req)
+				fmt.Println(rec.Code)
+				if rec.Code != tc.want {
+					t.Errorf("Expected: %d , got %d", tc.want, rec.Code)
+				}
 
-			if rec.Code != tc.want {
-				t.Errorf("Expected: %d , got %d", tc.want, rec.Code)
+			case "Add_Events_200":
+				data := url.Values{}
+				data.Add("event_id", "00000000-0000-0000-0000-000000000001")
+				data.Add("trip_id", "00000000-0000-0000-0000-000000000001")
+				models.MockedAddEventToTrip()
+				rec := httptest.NewRecorder()
+				req, _ := http.NewRequest(http.MethodPost, tc.url, bytes.NewBufferString(data.Encode()))
+				req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+				router.ServeHTTP(rec, req)
+				fmt.Println(rec.Code)
+				if rec.Code != tc.want {
+					t.Errorf("Expected: %d , got %d", tc.want, rec.Code)
+				}
 			}
 		})
 	}
@@ -78,15 +159,36 @@ func TestGetByTripHandler(t *testing.T) {
 			want:            200,
 			mockedGetEvents: []models.Event{},
 		},
+		{
+			name:            "Get_Events_200",
+			url:             "/v1/events/trip/sadsad",
+			want:            400,
+			mockedGetEvents: []models.Event{},
+		},
+		{
+			name:            "Get_Events_404",
+			url:             "/v1/events/trip/00000000-0000-0000-0000-000000000009",
+			want:            404,
+			mockedGetEvents: []models.Event{},
+		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			models.MockedGetEventsByTrip()
+			if tc.name == "Get_Events_404" {
+				mock := func() {
+					var err = http.ErrLineTooLong
+					models.GetEventsByTrip = func(trip_id uuid.UUID) ([]models.Event, error) {
+						return []models.Event{}, err
+					}
+				}
+				mock()
+			}
 			rec := httptest.NewRecorder()
 			req, _ := http.NewRequest(http.MethodGet, tc.url, nil)
 
 			router.ServeHTTP(rec, req)
-
+			fmt.Println(rec.Code)
 			if rec.Code != tc.want {
 				t.Errorf("Expected: %d , got %d", tc.want, rec.Code)
 			}
