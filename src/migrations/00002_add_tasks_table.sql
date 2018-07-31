b53f77b4104fe1b69619883515f17dff8041903b
Andriana <37633606+prudyvusandriana@users.noreply.github.com>
2018-07-31 22:34:38 +0300

    gooseUp&gooseDown

---
+++ src/migrations/00002_add_tasks_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE tasks (
+id uuid DEFAULT uuid_generate_v1(),
+user_id uuid REFERENCES users (id) ON DELETE CASCADE,
+name VARCHAR(34) NOT NULL,
+time TIMESTAMP,
+created_at TIMESTAMP,
+updated_at TIMESTAMP,
+description TEXT,
+PRIMARY KEY(id)
+);
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE tasks;
