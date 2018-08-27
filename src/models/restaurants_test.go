bcd153a0e58b2b865bd3e8545ccd5ce5215b35b7
Unknown <danyliukandrii1408@gmail.com>
2018-08-27 22:49:40 +0300

    test cleanup

--- src/models/restaurants_test.go
+++ src/models/restaurants_test.go
@@ -9,16 +9,6 @@ import (
 
 var restaurantsMockErr error
 
-type restaurantsTestCase struct {
-	name string
-	url  string
-}
-type reqGenTestCases struct {
-	name      string
-	paramName string
-	expected  string
-}
-
 func TestAddRestaurantToTrip(t *testing.T) {
 	originalDB := database.DB
 	database.DB, mock, restaurantsMockErr = sqlmock.New()
@@ -40,54 +30,3 @@ func TestAddRestaurantToTrip(t *testing.T) {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
-
-//func TestGetRestaurantByTripID(t *testing.T) {
-//	originalDB := database.DB
-//	database.DB, mock, flightMockErr = sqlmock.New()
-//	defer func() { database.DB = originalDB }()
-//
-//	ID, _ := uuid.FromString("00000000-0000-0000-0000-000000000001")
-//
-//	expected := []Restaurant{
-//		{
-//			ID,
-//			"Kryva Lypa",
-//			"Lviv",
-//			4,
-//			3,
-//			"Some info 1",
-//		},
-//		{
-//			ID,
-//			"Kryva Lypa",
-//			"Lviv",
-//			4,
-//			3,
-//			"Some info 1",
-//		},
-//	}
-//	if flightMockErr != nil {
-//		t.Fatalf("an error '%s' was not expected when opening a stub database connection", flightMockErr)
-//	}
-//
-//	rows := sqlmock.NewRows([]string{"ID", "name", "location", "stars", "prices", "description"}).
-//		AddRow(ID.Bytes(), expected[0].Name, expected[0].Location, expected[0].Stars, expected[0].Prices, expected[0].Description)
-//
-//	mock.ExpectQuery("SELECT (.+) FROM restaurants").WithArgs(ID).WillReturnRows(rows)
-//
-//	result, err := GetRestaurantsFromTrip(ID)
-//	fmt.Println(result)
-//
-//	if err != nil {
-//		t.Errorf("error was not expected while updating stats: %s", err)
-//	}
-//	if err := mock.ExpectationsWereMet(); err != nil {
-//		t.Errorf("there were unfulfilled expectations: %s", err)
-//	}
-//
-//	for i := 0; i < len(result); i++ {
-//		if expected[i] != result[i] {
-//			t.Error("Expected:", expected[i], "Was:", result[i])
-//		}
-//	}
-//}
