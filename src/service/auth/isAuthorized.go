573c0f255b473d003df6cfa64eec3840b2d0817d
andriana <anaprud@gmail.com>
2018-08-29 14:05:53 +0300

    comments added

--- src/service/auth/isAuthorized.go
+++ src/service/auth/isAuthorized.go
@@ -3,7 +3,6 @@ package auth
 import (
 	"github.com/Nastya-Kruglikova/cool_tasks/src/database"
 	"github.com/Nastya-Kruglikova/cool_tasks/src/service/common"
-	"log"
 	"net/http"
 )
 
@@ -22,12 +21,12 @@ func IsAuthorized(w http.ResponseWriter, r *http.Request, next http.HandlerFunc)
 	}
 	userSession, err := GetSessionIDFromRequest(w, r)
 	if err != nil {
+		common.SendError(w, r, 400, "ERROR: Can't get cookies", err)
 		return
 	}
 	if IsExistRedis(userSession) {
 		next(w, r)
 	} else {
-		log.Println(err, "ERROR: Not Authorized")
 		common.SendError(w, r, 401, "ERROR: Not Authorized", err)
 	}
 }
